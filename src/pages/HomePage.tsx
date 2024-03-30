0274f723aae1b7fab42cf3bb9bb1ae729d53951e
Niyati M Patel <patelnitie@gmail.com>
2024-03-29 20:29:17 -0400

    updated home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,10 +3,15 @@ import * as apiClient from "../axios/api-client";
 import Home from "../components/home/Home";
 
 const HomePage = () => {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["fetchAllMyHotels"],
     queryFn: apiClient.fetchHotels,
   });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   if (!data || data.length === 0) {
     return <span>No Hotels Found</span>;
   }
